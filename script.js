70467cb31a4ccc1463c94d25982da206e6822cfe
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-03-05 15:41:10 +0500

    added js scripts

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ recognition.interimResults = true;
 let p = document.createElement("p");
 const words = document.querySelector(".words");
 words.appendChild(p);
+recognition.lang = 'en-US';
 
 recognition.addEventListener("result", e => {
-  console.log(e);
+  const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join("")
+
+  p.textContent = transcript
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+    words.appendChild(p);
+  }
+  if (transcript.includes("get the weather")) {
+    console.log("🔥");
+  }
 })
+
+recognition.addEventListener("end", recognition.start)
 
 recognition.start()
